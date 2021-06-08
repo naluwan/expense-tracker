9b09b16d6f83171ada4629eff59d704a0cac97f8
naluwan <street.baller06@gmail.com>
2021-06-08 14:57:53 +0800

    feat: add mongoose config

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+require('./config/mongoose')
+
+
+
 app.get('/', (req, res) => {
   res.send(`This will be expense-tracker`)
 })
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Express is running on http://localhost:${PORT}`)
---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useCreateIndex: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log(`mongoose error!`)
+})
+
+db.once('open', () => {
+  console.log(`mongoose connected!`)
+})
+
+module.exports = db
